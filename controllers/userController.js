670126bfa3d16755c45bd580bd9ab92293936929
life2rich2025-commits <amurali123@gmail.com>
2025-12-13 21:27:41 +0530

    add email

--- controllers/userController.js
+++ controllers/userController.js
@@ -195,7 +195,7 @@ exports.forgotPassword = async (req, res) => {
   try {
     const { email } = req.body;
 
-    const user = await User.findOne({ email });
+    const user = await User.findOne({ email: email });
     if (!user)
       return res.status(404).json({ message: "User not found" });
 
