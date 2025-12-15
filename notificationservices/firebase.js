8f21c4134b2b7d092d3b984867ab0c4a2625a3e7
life2rich2025-commits <amurali123@gmail.com>
2025-12-15 20:33:45 +0530

    add key

--- notificationservices/firebase.js
+++ notificationservices/firebase.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const admin = require("firebase-admin");
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT.replace(/\\n/g, "\n"));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
